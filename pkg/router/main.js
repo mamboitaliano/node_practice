1790eeec77aa270ef83d1ad4a6334ba1a06dcfe5
raidersofthenight <ianemailaddress@gmail.com>
2015-08-06 00:45:59 -0700

    got data for dishwashers to render, fixing other routes

--- pkg/router/main.js
+++ pkg/router/main.js
@@ -6,55 +6,57 @@ module.exports = function(app) {
     app.get('/dishwashers', function(req, res) {
     	var dishwashers = "https://data.energystar.gov/resource/energy-star-certified-residential-dishwashers.json";
     	console.log("dishwasher route getting hit")
-    	var dishlist = {};
+    	var dishlist = undefined;
     	request(dishwashers, function (error, response, list) {
     		if (!error && response.statusCode == 200) {
-    			console.log(list); // print energystar dishwasher list json to console
-    			dishlist = JSON.parse(list);
+    			// console.log((JSON.parse(list)));
+    			formatted_list = (JSON.parse(list));
+    			dishlist = formatted_list
     		}
+    		res.render('dishwashers', {list: dishlist});
     	})
-    	res.render('dishwashers', {list: dishlist});
+    	
     });
 
     app.get('/washing_machines', function(req, res) {
     	var washing_machines = "https://data.energystar.gov/resource/energy-star-certified-residential-clothes-washers.json";
     	console.log("washing machine route getting hit");
-    	var washing_machine_list = {};
+    	var washing_machine_list = undefined;
     	request(washing_machines, function (error, response, list) {
     		if (!error && response.statusCode == 200) {
-    			console.log(list); // print energystar washing machine list json to console
+    			formatted_list = (JSON.parse(list));
     			washing_machine_list = list;
     		}
+    		res.render('washing_machines', {list: washing_machine_list});
     	})
-    	res.render('washing_machines', {list: washing_machine_list});
     	// why isn't this getting passed to washing_machines.ejs?
     });
 
     app.get('/displays', function(req, res) {
     	var displays = "https://data.energystar.gov/resource/energy-star-certified-displays.json"
     	console.log("washing machine route getting hit");
-    	var display_list = {};
+    	var display_list = undefined;
     	request(displays, function (error, response, list) {
     		if (!error && response.statusCode == 200) {
-    			console.log(list); // print energystar washing machine list json to console
+    			formatted_list = (JSON.parse(list));
     			display_list = list;
     		}
+    		res.render('displays', {list: display_list});
     	})
-    	res.render('displays', {list: display_list});
     	// why isn't this getting passed to dishwashers.ejs?
     });
 
     app.get('/audio_video', function(req, res) {
     	var audio_video = "https://data.energystar.gov/resource/energy-star-certified-audio-video.json";
     	console.log("audio_video route getting hit");
-    	var audio_video_list = {};
+    	var audio_video_list = undefined;
     	request(audio_video, function (error, response, list) {
     		if (!error && response.statusCode == 200) {
-    			console.log(list); // print energystar washing machine list json to console
+    			formatted_list = (JSON.parse(list));
     			audio_video_list = list;
     		}
+    		res.render('audio_video', {list: audio_video_list});
     	})
-    	res.render('audio_video', {list: audio_video_list});
     	// why isn't this getting passed to dishwashers.ejs?
     });
 
@@ -64,11 +66,11 @@ module.exports = function(app) {
     	var water_heater_list = {};
     	request(water_heaters, function (error, response, list) {
     		if (!error && response.statusCode == 200) {
-    			console.log(list); // print energystar water heater list json to console
+    			formatted_list = (JSON.parse(list));
     			water_heater_list = list;
     		}
+    		res.render('water_heaters', {list: water_heater_list});
     	})
-    	res.render('water_heaters', {list: water_heater_list});
     	// why isn't this getting passed to dishwashers.ejs?
     });
 
@@ -78,11 +80,11 @@ module.exports = function(app) {
     	var refrigerator_list = {};
     	request(refrigerators, function (error, response, list) {
     		if (!error && response.statusCode == 200) {
-    			console.log(list); // print energystar refrigerator list json to console
+    			formatted_list = (JSON.parse(list));
     			refrigerator_list = list;
     		}
+    		res.render('refrigerators', {list: refrigerator_list});
     	})
-    	res.render('refrigerators', {list: refrigerator_list});
     	// why isn't this getting passed to dishwashers.ejs?
     });
 
@@ -92,11 +94,11 @@ module.exports = function(app) {
     	var freezer_list = {};
     	request(freezers, function (error, response, list) {
     		if (!error && response.statusCode == 200) {
-    			console.log(list); // print energystar freezer list json to console
+    			formatted_list = (JSON.parse(list));
     			freezer_list = list;
     		}
+    		res.render('freezers', {list: freezer_list});
     	})
-    	res.render('freezers', {list: freezer_list});
     	// why isn't this getting passed to dishwashers.ejs?
     });
 
